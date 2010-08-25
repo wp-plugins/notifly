95f4ce49688f8ce48ff95a34705a66f67999270f
johnjamesjacoby <johnjamesjacoby@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-25 02:31:46 +0000

    Update plugin description.
    git-svn-id: https://plugins.svn.wordpress.org/notifly/trunk@280425 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- notifly.php
+++ notifly.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Notifly - Post/Comment Emailer
 Plugin URI: http://wordpress.org/extend/plugins/notifly/
-Description: Sends a notification to all users of a site when a new post or comment is made. Add email addesses in your Discussion Settings area.
+Description: Sends an email to the addresses of your choice when a new post or comment is made. Add email addesses in your Discussion Settings area.
 Author: Otto42, Matt, John James Jacoby
 Version: 1.2.2
 Author URI: http://ottodestruct.com
