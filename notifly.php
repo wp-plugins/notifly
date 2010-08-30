44b882d8a3ae89fc2b1bd7caf7d2ca796cf30481
johnjamesjacoby <johnjamesjacoby@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-30 08:33:09 +0000

    Bump to 1.2.5. Add preliminary HTML email support.
    git-svn-id: https://plugins.svn.wordpress.org/notifly/trunk@282510 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- notifly.php
+++ notifly.php
@@ -4,7 +4,7 @@ Plugin Name: Notifly
 Plugin URI: http://wordpress.org/extend/plugins/notifly/
 Description: Sends an email to the addresses of your choice when a new post or comment is made. Add email addresses in your Discussion Settings area.
 Author: Otto42, Matt, John James Jacoby
-Version: 1.2.4
+Version: 1.2.5
 Author URI: http://ottodestruct.com
 */
 
@@ -182,20 +182,19 @@ function pce_comment_email( $comment_id, $comment_status ) {
 	// Email from address
 	$wp_email = 'wordpress@' . preg_replace( '#^www\.#', '', strtolower( $_SERVER['SERVER_NAME'] ) );
 
-	// Email Subject
-	$email['subject'] = sprintf( __( '[%1$s] Comment: "%2$s"', 'notifly' ), $blogname, $post->post_title );
+	// Content details
+	$message['permalink']     = $permalink;
+	$message['shortlink']     = wp_get_shortlink( $post->ID, 'post' ) . '#comment-' . $comment_id;
+	$message['timestamp']     = sprintf( __( '%1$s at %2$s', 'notifly' ), get_post_time( 'F j, Y', false, $post ), get_post_time( 'g:i a', false, $post ) );
+	$message['title']         = $post->post_title;
+	$message['author_name']   = $comment->comment_author;
+	$message['author_link']   = $comment->comment_author_url;
+	$message['author_avatar'] = pce_get_avatar( $comment->comment_author_email );
+	$message['content']       = strip_tags( $comment->comment_content );
 
 	// Email Body
-	$message['post_title']        = sprintf( __( 'New comment on post "%s"', 'notifly' ), $post->post_title );
 	$message['comment_author']    = sprintf( __( 'Author : %1$s (IP: %2$s , %3$s)', 'notifly' ), $comment->comment_author, $comment->comment_author_IP, $comment_author_domain );
-	$message['comment_email']     = sprintf( __( 'E-mail : %s', 'notifly' ), $comment->comment_author_email );
-	$message['comment_url']       = sprintf( __( 'URL    : %s', 'notifly' ), $comment->comment_author_url );
 	$message['comment_whois']     = sprintf( __( 'Whois  : http://ws.arin.net/cgi-bin/whois.pl?queryinput=%s', 'notifly' ), $comment->comment_author_IP );
-	$message['comment_content']   = sprintf( __( 'Comment: %s', 'notifly' ), "<br />" . $comment->comment_content . "<br />" );
-	$message['comment_permalink'] = sprintf( __( 'You can see all comments on this post here: %s', 'notifly' ), $permalink );
-
-	foreach ( $message as $message_part )
-		$email['body'] .= $message_part . "<br /><br />";
 
 	// Email Headers
 	$headers['from'] = sprintf( 'From: %1$s <%2$s>', !empty( $comment->comment_author ) ? $comment->comment_author : $blogname, $wp_email );
@@ -204,14 +203,15 @@ function pce_comment_email( $comment_id, $comment_status ) {
 	if ( !empty( $comment->comment_author_email ) )
 		$headers['reply-to'] = sprintf( 'Reply-To: %1$s <%2$s>', $comment->comment_author_email, $comment->comment_author_email );
 
+	// Email Subject
+	$email['recipients'] = pce_get_recipients( array( $author->user_email, $comment->comment_author_email ) );
+	$email['subject']    = sprintf( __( '[%1$s] Comment: "%2$s"', 'notifly' ), $blogname, $post->post_title );
+	$email['body']       = pce_get_html_email_template( 'post', $message );
 	foreach ( $headers as $header_part )
 		$email['headers'] .= $header_part . "\n";
 
-	// Get recipients
-	$recipients = pce_get_recipients( array( $author->user_email, $comment->comment_author_email ) );
-
 	// Send email to each user
-	foreach ( $recipients as $recipient )
+	foreach ( $email['recipients'] as $recipient )
 		@wp_mail( $recipient, $email['subject'], $email['body'], $email['headers'] );
 }
 add_action( 'comment_post', 'pce_comment_email', 10, 2 );
@@ -238,21 +238,19 @@ function pce_post_email( $new, $old, $post ) {
 	// Get comment info
 	$blogname   = wp_specialchars_decode( get_option( 'blogname' ), ENT_QUOTES );
 	$permalink  = get_permalink( $post->ID );
+	$wp_email   = 'wordpress@' . preg_replace( '#^www\.#', '', strtolower( $_SERVER['SERVER_NAME'] ) );
 
-	// Email from address
-	$wp_email = 'wordpress@' . preg_replace( '#^www\.#', '', strtolower( $_SERVER['SERVER_NAME'] ) );
-
-	// Email Subject
-	$email['subject'] = sprintf( __( '[%1$s] Post: "%2$s" by %3$s' ), $blogname, $post->post_title, $author->user_nicename );
-
-	// Email body
-	$message['permalink']    = get_permalink( $post->ID );
-	$message['post_title']   = sprintf( __( 'Title: "%s"', 'notifly'  ), $post->post_title );
-	$message['post_author']  = sprintf( __( 'Author : %s', 'notifly'  ), $author->user_nicename );
-	$message['post_preview'] = sprintf( __( 'Post Preview: %s', 'notifly' ), "<br />" . strip_tags( $post->post_content ) );
-
-	foreach ( $message as $message_part )
-		$email['body'] .= $message_part . "<br /><br />";
+	// Content details
+	$message['permalink']     = get_permalink( $post->ID );
+	$message['shortlink']     = wp_get_shortlink( $post->ID, 'post' );
+	$message['timestamp']     = sprintf( __( '%1$s at %2$s', 'notifly' ), get_post_time( 'F j, Y', false, $post ), get_post_time( 'g:i a', false, $post ) );
+	$message['title']         = $post->post_title;
+	$message['author_name']   = $author->user_nicename;
+	$message['author_link']   = get_author_link( false, $author->user_id );
+	$message['author_avatar'] = pce_get_avatar( $author->user_email );
+	$message['tags']          = wp_get_post_tags( $post->ID );
+	$message['categories']    = wp_get_post_categories( $post->ID );
+	$message['content']       = strip_tags( $post->post_content );
 
 	// Headers
 	$headers['from'] = sprintf( 'From: %1$s <%2$s>', $blogname, $wp_email );
@@ -261,16 +259,199 @@ function pce_post_email( $new, $old, $post ) {
 	if ( !empty( $user->user_email ) )
 		$headers['reply-to'] = sprintf( 'Reply-To: %1$s <%2$s>', $user->user_email, $user->user_email );
 
+	// Create the email
+	$email['subject']    = sprintf( __( '[%1$s] Post: "%2$s" by %3$s' ), $blogname, $post->post_title, $author->user_nicename );
+	$email['body']       = pce_get_html_email_template( 'post', $message );
+	$email['recipients'] = pce_get_recipients( array( $author->user_email ) );
 	foreach ( $headers as $header_part )
 		$email['headers'] .= $header_part . "\n";
 
-	// Get recipients
-	$recipients = pce_get_recipients( array( $author->user_email ) );
-
 	// Send email to each user
-	foreach ( $recipients as $recipient )
+	foreach ( $email['recipients'] as $recipient )
 		@wp_mail( $recipient, $email['subject'], $email['body'], $email['headers'] );
 }
 add_action( 'transition_post_status', 'pce_post_email', 10, 3 );
+
+/**
+ * pce_post_template()
+ *
+ * Template used for Notifly emails
+ */
+function pce_get_html_email_template( $type, $args ) {
+
+	// Build the post meta
+	$meta = '| ' . $args['timestamp'];
+	if ( $args['tags'] ) {
+		//$meta .= ' | ' . __( 'Tags:', 'notifly' );
+		//<a href="" style="text-decoration: none; color: #0088cc;"></a>
+	}
+	if ( $args['categories'] ) {
+		//$meta .= ' | ' . __( 'Categories:', 'notifly' );
+		//<a href="" style="text-decoration: none; color: #0088cc;"></a>
+	}
+	$meta .= ' | ' . __( 'Short Link:', 'notifly' ) . ' <a href="' . $args['shortlink'] . '" style="text-decoration: none; color: #0088cc;">' . $args['shortlink'] . '</a>';
+
+	// Build the email pieces
+	$email['doctype'] = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">';
+	$email['html']    = '<html xmlns="http://www.w3.org/1999/xhtml">';
+	$email['head']    = '<head>
+		<style type="text/css" media="all">
+		a:hover { color: red; }
+		a {
+			text-decoration: none;
+			color: #0088cc;
+		}
+		@media only screen and (max-device-width: 480px) {
+			 .post { min-width: 700px !important; }
+		}
+		</style>
+		<title><?php // blog title ?></title>
+		<!--[if gte mso 12]>
+		<style type="text/css" media="all">
+		body {
+		font-family: arial;
+		font-size: 0.8em;
+		}
+		.post, .comment {
+		background-color: white !important;
+		line-height: 1.4em !important;
+		}
+		</style>
+		<![endif]-->
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	</head>';
+
+	// Type of notification determines the body
+	if ( 'post' == $type ) {
+
+		// Email Body
+		$email['body'] = '<body>
+		<div style="max-width: 1024px; min-width: 600px;" class="content">
+			<div style="padding: 1em; margin: 1.5em 1em 0.5em 1em; background-color: #f5f5f5; border: 1px solid #ccc; -webkit-border-radius: 5px; -moz-border-radius: 5px; border-radius: 5px; line-height: 1.6em;" class="post">
+				<table style="width: 100%;" class="post-details">
+					<tr>
+						<!-- Author Avatar -->
+						<td valign="top"  style="width: 48px; margin-right: 7px;" class="table-avatar">
+							<a href="' . $args['author_link'] . '" style="text-decoration: none; color: #0088cc;">
+								' . $args['author_avatar'] . '
+							</a>
+						</td>
+
+						<td valign="top">
+							<!-- Post Title -->
+							<h2 style="margin: 0; font-size: 1.6em; color: #555;" class="post-title">
+								<a href="' . $args['permalink'] . '" style="text-decoration: none; color: #0088cc;">' . $args['title'] . '</a>
+							</h2>
+
+							<!-- Author Info -->
+							<div style="color: #999; font-size: 0.9em; margin-top: 4px;" class="meta">
+								<strong>
+									<a href="' . $args['author_link'] . '" style="text-decoration: none; color: #0088cc;">' . $args['author_name'] . '</a>
+								</strong>
+								<!-- Post Meta -->
+								' . $meta . '
+							</div>
+						</td>
+					</tr>
+				</table>
+
+				<!-- Post -->
+				<p>' . $args['content'] . '</p>
+				<div style="clear: both"></div>
+
+				<!-- Reply -->
+				<p style="margin-bottom: 0.4em">
+					<a href="' . $args['permalink'] . '#respond" style="text-decoration: none; color: #0088cc;">' . __( 'Add a comment to this post', 'notifly' ) . '</a>
+				</p>
+			</div>
+		</div>
+
+		<!-- WordPress Logo -->
+		<table style="max-width: 1024px; min-width: 600px; line-height: 1.6em;" class="footer">
+			<tr>
+				<td width="80">
+					<img border="0" src="http://s.wordpress.org/about/images/logo-grey/grey-m.png" alt="WordPress" width="64" height="64" />
+				</td>
+			</tr>
+		</table>
+	</body>';
+
+	// Comment Notification
+	} elseif ( 'comment' == $type ) {
+
+	}
+
+	$email['bottom'] = '</html>';
+
+	// Compile email content
+	foreach ( $email as $email_part )
+		$email_complete .= $email_part . "\n";
+
+	return $email_complete;
+}
+
+/**
+ * pce_get_avatar( $email = '' )
+ *
+ * Custom get_avatar function with inline styling
+ *
+ * @param string $email optional
+ * @return string <img>
+ */
+function pce_get_avatar( $email = '' ) {
+
+	// Set avatar size
+	$size = '48';
+
+	// Get site default avatar
+	$avatar_default = get_option( 'avatar_default' );
+	if ( empty( $avatar_default ) )
+		$default = 'mystery';
+	else
+		$default = $avatar_default;
+
+	$email_hash = md5( strtolower( $email ) );
+
+	// SSL?
+	if ( is_ssl() ) {
+		$host = 'https://secure.gravatar.com';
+	} else {
+		if ( !empty($email) )
+			$host = sprintf( "http://%d.gravatar.com", ( hexdec( $email_hash{0} ) % 2 ) );
+		else
+			$host = 'http://0.gravatar.com';
+	}
+
+	// Get avatar/gravatar
+	if ( 'mystery' == $default )
+		$default = "$host/avatar/ad516503a11cd5ca435acc9bb6523536?s={$size}"; // ad516503a11cd5ca435acc9bb6523536 == md5('unknown@gravatar.com')
+	elseif ( 'blank' == $default )
+		$default = includes_url( 'images/blank.gif' );
+	elseif ( !empty( $email ) && 'gravatar_default' == $default )
+		$default = '';
+	elseif ( 'gravatar_default' == $default )
+		$default = "$host/avatar/s={$size}";
+	elseif ( empty( $email ) )
+		$default = "$host/avatar/?d=$default&amp;s={$size}";
+	elseif ( strpos( $default, 'http://' ) === 0 )
+		$default = add_query_arg( 's', $size, $default );
+
+	if ( !empty( $email ) ) {
+		$out = "$host/avatar/";
+		$out .= $email_hash;
+		$out .= '?s='.$size;
+		$out .= '&amp;d=' . urlencode( $default );
+
+		$rating = get_option('avatar_rating');
+		if ( !empty( $rating ) )
+			$out .= "&amp;r={$rating}";
+
+		$avatar = '<img alt="' . $safe_alt . '" src="' . $out . '" style="border: 1px solid #ddd; padding: 2px; background-color: white; width: 48px; margin-right: 7px;" class="avatar avatar-' . $size . ' photo" height="' . $size . '" width="' . $size . '" />';
+	} else {
+		$avatar = '<img alt="' . $safe_alt . '" src="' . $default . '" style="border: 1px solid #ddd; padding: 2px; background-color: white; width: 48px; margin-right: 7px;" class="avatar avatar-default avatar-' . $size . ' photo" height="' . $size . '" width="' . $size . '" />';
+	}
+
+	return apply_filters( 'pce_get_avatar', $avatar, $id_or_email, $size, $default, $alt );
+}
 
 ?>
