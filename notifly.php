89e9dff46862da3e4a1afffe46c11d8d1d280b1d
johnjamesjacoby <johnjamesjacoby@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-09-17 08:51:47 +0000

    git-svn-id: https://plugins.svn.wordpress.org/notifly/trunk@290553 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- notifly.php
+++ notifly.php
@@ -179,9 +179,10 @@ class Notifly {
 						unset( $recipients[$key] );
 					}
 				}
-				$recipients = array_values( $recipients );
 			}
 		}
+
+		$recipients = array_values( $recipients );
 
 		// Return result
 		return apply_filters( 'notifly_get_recipients', $recipients );
@@ -199,36 +200,39 @@ class Notifly {
 	function comment_email( $comment_id, $comment_status ) {
 
 		// Only send emails for actual published comments
-		if ( '1' == $comment_status || 'approve' == $comment_status ) {
+		switch ( $comment_status ) {
+			case '1' :
+			case 'approve' :
+				$comment                   = get_comment( $comment_id );
+				$post                      = get_post( $comment->comment_post_ID );
+				$post_author               = get_userdata( $post->post_author );
+				$comment_author_domain     = gethostbyaddr( $comment->comment_author_IP );
 
-			$comment                   = get_comment( $comment_id );
-			$post                      = get_post( $comment->comment_post_ID );
-			$post_author               = get_userdata( $post->post_author );
-			$comment_author_domain     = gethostbyaddr( $comment->comment_author_IP );
+				// Content details
+				$message['permalink']      = get_permalink( $comment->comment_post_ID ) . '#comment-' . $comment_id;
+				$message['shortlink']      = wp_get_shortlink( $post->ID, 'post' ) . '#comment-' . $comment_id;
+				$message['timestamp']      = sprintf( __( '%1$s at %2$s', 'notifly' ), get_post_time( 'F j, Y', false, $post ), get_post_time( 'g:i a', false, $post ) );
+				$message['title']          = $post->post_title;
+				$message['author_name']    = $comment->comment_author;
+				$message['author_link']    = $comment->comment_author_url;
+				$message['author_avatar']  = $this->get_avatar( $comment->comment_author_email );
+				$message['content']        = strip_tags( $comment->comment_content );
 
-			// Content details
-			$message['permalink']      = get_permalink( $comment->comment_post_ID ) . '#comment-' . $comment_id;
-			$message['shortlink']      = wp_get_shortlink( $post->ID, 'post' ) . '#comment-' . $comment_id;
-			$message['timestamp']      = sprintf( __( '%1$s at %2$s', 'notifly' ), get_post_time( 'F j, Y', false, $post ), get_post_time( 'g:i a', false, $post ) );
-			$message['title']          = $post->post_title;
-			$message['author_name']    = $comment->comment_author;
-			$message['author_link']    = $comment->comment_author_url;
-			$message['author_avatar']  = $this->get_avatar( $comment->comment_author_email );
-			$message['content']        = strip_tags( $comment->comment_content );
+				// Comment Extras
+				$message['comment_author'] = sprintf( __( 'Author : %1$s (IP: %2$s , %3$s)', 'notifly' ), $comment->comment_author, $comment->comment_author_IP, $comment_author_domain );
+				$message['comment_whois']  = sprintf( __( 'Whois  : http://ws.arin.net/cgi-bin/whois.pl?queryinput=%s', 'notifly' ), $comment->comment_author_IP );
 
-			// Comment Extras
-			$message['comment_author'] = sprintf( __( 'Author : %1$s (IP: %2$s , %3$s)', 'notifly' ), $comment->comment_author, $comment->comment_author_IP, $comment_author_domain );
-			$message['comment_whois']  = sprintf( __( 'Whois  : http://ws.arin.net/cgi-bin/whois.pl?queryinput=%s', 'notifly' ), $comment->comment_author_IP );
+				// Email Subject
+				$email['subject']          = sprintf( __( '[%1$s] Comment: "%2$s"', 'notifly' ), $this->blogname, $post->post_title );
+				$email['recipients']       = $this->get_recipients();
+				$email['body']             = $this->get_html_email_template( 'post', $message );
+				$email['headers']          = $this->get_email_headers( sprintf( 'Reply-To: %1$s <%2$s>', __( 'No Reply', 'notifly' ), $this->no_reply_to() ) );
 
-			// Email Subject
-			$email['subject']          = sprintf( __( '[%1$s] Comment: "%2$s"', 'notifly' ), $this->blogname, $post->post_title );
-			$email['recipients']       = $this->get_recipients();
-			$email['body']             = $this->get_html_email_template( 'post', $message );
-			$email['headers']          = $this->get_email_headers( sprintf( 'Reply-To: %1$s <%2$s>', $comment->comment_author_email, $comment->comment_author_email ) );
+				// Send email to each user
+				foreach ( (array)$email['recipients'] as $recipient )
+					@wp_mail( $recipient, $email['subject'], $email['body'], $email['headers'] );
 
-			// Send email to each user
-			foreach ( (array)$email['recipients'] as $recipient )
-				@wp_mail( $recipient, $email['subject'], $email['body'], $email['headers'] );
+				break;
 		}
 	}
 
@@ -272,6 +276,17 @@ class Notifly {
 		// Send email to each user
 		foreach ( $email['recipients'] as $recipient )
 			@wp_mail( $recipient, $email['subject'], $email['body'], $email['headers'] );
+	}
+
+	/**
+	 * no_reply_to()
+	 *
+	 * Hide comment author email address from going out to everyone
+	 *
+	 * @return string
+	 */
+	function no_reply_to() {
+		return apply_filters( 'notifly_no_reply_to', 'noreply@' . preg_replace( '#^www\.#', '', strtolower( $_SERVER['SERVER_NAME'] ) ) );
 	}
 
 	/**
